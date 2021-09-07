9e11f7cce60bf99e3f9572bd00db7ab35ef370d8
m.dehbashi <Mohsen.Dehbashi@digikala.com>
2021-09-07 16:23:58 +0430

    Theme and app's basic layout added

---
+++ src/main/kotlin/ui/Theme.kt
@@ -0,0 +1,43 @@
+package ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+private val colors = lightColors(
+    primary = Color(0xFFF44336),
+    primaryVariant = Color(0xFFCC372C),
+    secondary = Color(0xFFFF9800),
+    onSecondary = Color.White
+)
+
+private val shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
+
+private val typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
+
+@Composable
+fun DigikalaTheme(content: @Composable () -> Unit) = MaterialTheme(
+    colors = colors,
+    shapes = shapes,
+    typography = typography,
+    content = content
+)
